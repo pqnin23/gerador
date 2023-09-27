6baaeade8a397097dc7dba52e806ab4d10e67337
pqnin23 <128159590+pqnin23@users.noreply.github.com>
2023-09-27 12:20:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321";
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+function generatePassword(){
+    let pass = '';
+    for(){
+        
+    }
+}
